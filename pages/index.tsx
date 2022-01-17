f538a4d645710ca9dba9a65d2deeae1175e45114
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-01-17 07:55:20 +0000

    User delete

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import { memo, VFC } from "react";
 const Login: VFC = memo(() => {
   return (
     <>
-      <Link href={"/home"}>home</Link>
-      <br />
       <Link href={"/practice"}>practice</Link>
     </>
   );
